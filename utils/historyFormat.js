80fdf357d30a3f936379cb3f52f9e51316b02ab8
Harsh <harshpatel120605@gmail.com>
2024-09-05 03:36:07 +0530

    some modifications

--- utils/historyFormat.js
+++ utils/historyFormat.js
@@ -2,9 +2,9 @@ export default function formatConvHistory(messages) {
   return messages
     ?.map((message, i) => {
       if (i % 2 === 0) {
-        return `AI: ${message}`;
-      } else {
         return `HUMAN: ${message}`;
+      } else {
+        return `AI: ${message}`;
       }
     })
     .join('\n');
